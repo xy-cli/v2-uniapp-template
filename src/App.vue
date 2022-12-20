f7325a024c3b60f1d1bcd1222f8b734eec99bc11
ss <shangs@schbrain.com>
2022-12-20 17:37:05 +0800

    feat: 注释、tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: ss shangs@schbrain.com
- * @Date: 2022-07-15 14:02:06
- * @LastEditors: ss shangs@schbrain.com
- * @LastEditTime: 2022-08-08 11:12:13
- * @FilePath: /v2-uni-app/src/App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <script>
 // 测试2
 export default {
@@ -30,9 +22,9 @@ export default {
 /* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
 @import "uview-ui/index";
 body{
-  @include bis('./static/image/backgroundWhite.png')
+  //@include bis('./static/image/backgroundWhite.png')
   //background: linear-gradient(50%, #999, #444);
-  // background: linear-gradient(180deg, #999 0%, #555 100%);
+   background: linear-gradient(180deg, #999 0%, #555 100%);
 }
 page{
   background-color: $uni-color-bg;
--- src/main.js
+++ src/main.js
@@ -1,11 +1,3 @@
-/*
- * @Author: ss shangs@schbrain.com
- * @Date: 2022-05-23 11:24:30
- * @LastEditors: ss shangs@schbrain.com
- * @LastEditTime: 2022-08-08 10:48:23
- * @FilePath: /v2-uni-app/src/main.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import Vue from 'vue'
 import App from './App'
 import '@/utils/loading'
@@ -16,7 +8,6 @@ import store from '@/store'
 // 引入全局uView
 import uView from 'uview-ui'
 
-import Vconsole from 'vconsole'
 
 // http拦截器，将此部分放在new Vue()和app.$mount()之间，才能App.vue中正常使用
 import httpInterceptor from '@/common/http.interceptor.js'
@@ -32,10 +23,6 @@ App.mpType = 'app'
 const vuexStore = require('@/store/$u.mixin.js')
 Vue.mixin(vuexStore)
 Vue.use(uView)
-if (process.env.VUE_APP_ENV === 'qa') {
-  const vconsole = new Vconsole()
-  Vue.use(vconsole)
-}
 const app = new Vue({
   store,
   ...App
