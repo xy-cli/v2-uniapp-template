4e2273e25b36525cf316c40657e3446ce0a7c237
s-xianyu <s22539634@aliyun.com>
2024-06-14 16:45:09 +0800

    feat: 替换成网络图片

--- src/utils/tabBar.js
+++ src/utils/tabBar.js
@@ -1,20 +1,20 @@
 module.exports = [
   {
     pagePath: 'pages/home/index',
-    iconPath: '/static/image/home.png',
-    selectedIconPath: '/static/image/home-active.png',
+    iconPath: 'https://s21.ax1x.com/2024/06/14/pkdBqDP.png',
+    selectedIconPath: 'https://s21.ax1x.com/2024/06/14/pkdBbut.png',
     text: '首页',
     customIcon: false
   }, {
     pagePath: 'pages/statistics/index',
-    iconPath: '/static/image/home.png',
-    selectedIconPath: '/static/image/home-active.png',
+    iconPath: 'https://s21.ax1x.com/2024/06/14/pkdBqDP.png',
+    selectedIconPath: 'https://s21.ax1x.com/2024/06/14/pkdBbut.png',
     text: '统计',
     customIcon: false
   }, {
     pagePath: 'pages/profile/index',
-    iconPath: '/static/image/home.png',
-    selectedIconPath: '/static/image/home-active.png',
+    iconPath: 'https://s21.ax1x.com/2024/06/14/pkdBqDP.png',
+    selectedIconPath: 'https://s21.ax1x.com/2024/06/14/pkdBbut.png',
     text: '个人中心',
     customIcon: false
   }
